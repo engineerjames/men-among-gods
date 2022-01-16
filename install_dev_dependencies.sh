ed7c211bf296913f0560d083273c8bc63834641d
James Armes <jamesleearmes@gmail.com>
2022-01-15 18:45:34 -0700

    Add SFML dev dependency instructions

---
+++ install_dev_dependencies.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Installing development dependencies for SFML..."
+sudo apt-get install -y libx11-dev libxrandr-dev libopengl-dev libgl-dev libfreetype-dev libopenal-dev libvorbis-dev libflac-dev gdb
