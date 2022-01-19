59229357246f15210195f36a25fb87bd91457d2a
James Armes <jamesleearmes@gmail.com>
2022-01-19 13:35:43 -0700

    Format PlayerData.h

--- src/new_client/PlayerData.h
+++ src/new_client/PlayerData.h
@@ -14,12 +14,11 @@ public:
 
 private:
   // Holds the name, description, and some client-related settings (split apart later)
-  pdata     playerInfo_;
-  cplayer   clientSidePlayerInfo_;
-  key       okey_;
-  std::unique_ptr<skilltab[]> skillsList_;
-  std::unique_ptr<cmap[]> map_;
-    // map = static_cast< cmap * >( calloc( MAPX * MAPY * sizeof( struct cmap ), 1 ) );
+  pdata                         playerInfo_;
+  cplayer                       clientSidePlayerInfo_;
+  key                           okey_;
+  std::unique_ptr< skilltab[] > skillsList_;
+  std::unique_ptr< cmap[] >     map_;
 };
 
 #endif
