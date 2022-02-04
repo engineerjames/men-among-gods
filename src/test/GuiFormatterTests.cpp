ab21c38fc0655ec4d36205ce67ea61bf31cabb48
James Armes <jamesleearmes@gmail.com>
2022-02-04 14:48:26 -0700

    Added ThousandsSeparator tests

--- src/test/GuiFormatterTests.cpp
+++ src/test/GuiFormatterTests.cpp
@@ -48,3 +48,74 @@ BOOST_AUTO_TEST_CASE( GOLD_AND_SILVER )
 }
 
 BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE( THOUSANDS_SEPARATOR_TESTS )
+
+BOOST_AUTO_TEST_CASE( LESS_THAN_1000 )
+{
+  std::string expectedValue = "0";
+  std::string actualValue   = MenAmongGods::addThousandsSeparator( 0 );
+
+  BOOST_CHECK_EQUAL( expectedValue, actualValue );
+
+  expectedValue = "999";
+  actualValue   = MenAmongGods::addThousandsSeparator( 999 );
+
+  BOOST_CHECK_EQUAL( expectedValue, actualValue );
+}
+
+BOOST_AUTO_TEST_CASE( EXACTLY_1000 )
+{
+  std::string expectedValue = "1,000";
+  std::string actualValue   = MenAmongGods::addThousandsSeparator( 1000 );
+
+  BOOST_CHECK_EQUAL( expectedValue, actualValue );
+}
+
+BOOST_AUTO_TEST_CASE( OVER_9000 )
+{
+  std::string expectedValue = "9,998";
+  std::string actualValue   = MenAmongGods::addThousandsSeparator( 9998 );
+
+  BOOST_CHECK_EQUAL( expectedValue, actualValue );
+}
+
+BOOST_AUTO_TEST_CASE( OVER_10_000 )
+{
+  std::string expectedValue = "15,432";
+  std::string actualValue   = MenAmongGods::addThousandsSeparator( 15432 );
+
+  BOOST_CHECK_EQUAL( expectedValue, actualValue );
+}
+
+BOOST_AUTO_TEST_CASE( OVER_100_000 )
+{
+  std::string expectedValue = "186,326";
+  std::string actualValue   = MenAmongGods::addThousandsSeparator( 186326 );
+  BOOST_CHECK_EQUAL( expectedValue, actualValue );
+
+  expectedValue = "542,121";
+  actualValue   = MenAmongGods::addThousandsSeparator( 542121 );
+  BOOST_CHECK_EQUAL( expectedValue, actualValue );
+
+  expectedValue = "986,326";
+  actualValue   = MenAmongGods::addThousandsSeparator( 986326 );
+  BOOST_CHECK_EQUAL( expectedValue, actualValue );
+}
+
+BOOST_AUTO_TEST_CASE( NEGATIVE_NUMBERS )
+{
+  std::string expectedValue = "-186,326";
+  std::string actualValue   = MenAmongGods::addThousandsSeparator( -186326 );
+  BOOST_CHECK_EQUAL( expectedValue, actualValue );
+
+  expectedValue = "-999";
+  actualValue   = MenAmongGods::addThousandsSeparator( -999 );
+  BOOST_CHECK_EQUAL( expectedValue, actualValue );
+
+  expectedValue = "-100";
+  actualValue   = MenAmongGods::addThousandsSeparator( -100 );
+  BOOST_CHECK_EQUAL( expectedValue, actualValue );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
