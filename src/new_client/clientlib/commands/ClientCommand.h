9bd485cd0df6ce70a6a99bb5d1732176f1340460
James Armes <jamesleearmes@gmail.com>
2022-02-06 12:58:13 -0700

    Added additional send functions to base ClientCommand class

--- src/new_client/clientlib/commands/ClientCommand.h
+++ src/new_client/clientlib/commands/ClientCommand.h
@@ -15,6 +15,22 @@ public:
   virtual void send( sf::TcpSocket& socket ) const = 0;
 
 protected:
+  void sendOneArgument( sf::TcpSocket& socket, std::uint32_t x ) const
+  {
+    unsigned char buf[ 16 ] {};
+
+    buf[ 0 ] = static_cast< unsigned char >( CmdVal );
+
+    *( std::uint32_t* ) ( buf + 1 ) = x;
+
+    std::size_t bytesSent = 0;
+
+    while ( bytesSent != 16 )
+    {
+      socket.send( buf, sizeof( buf ), bytesSent );
+    }
+  }
+
   void sendTwoArguments( sf::TcpSocket& socket, std::uint16_t x, std::uint32_t y ) const
   {
     unsigned char buf[ 16 ] {};
@@ -23,6 +39,24 @@ protected:
 
     *( std::uint16_t* ) ( buf + 1 ) = x;
     *( std::uint32_t* ) ( buf + 3 ) = y;
+
+    std::size_t bytesSent = 0;
+
+    while ( bytesSent != 16 )
+    {
+      socket.send( buf, sizeof( buf ), bytesSent );
+    }
+  }
+
+  void sendThreeArguments( sf::TcpSocket& socket, std::uint32_t x, std::uint32_t y, std::uint32_t z ) const
+  {
+    unsigned char buf[ 16 ] {};
+
+    buf[ 0 ] = static_cast< unsigned char >( CmdVal );
+
+    *( std::uint32_t* ) ( buf + 1 ) = x;
+    *( std::uint32_t* ) ( buf + 5 ) = y;
+    *( std::uint32_t* ) ( buf + 9 ) = z;
 
     std::size_t bytesSent = 0;
 
