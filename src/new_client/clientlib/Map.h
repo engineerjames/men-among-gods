a71e44bbb2d367a8358d7d37205f284da20b29cc
James Armes <jamesleearmes@gmail.com>
2022-02-07 22:46:42 -0700

    Move most of map functionality into Map vs. MapDisplay

--- src/new_client/clientlib/Map.h
+++ src/new_client/clientlib/Map.h
@@ -20,10 +20,21 @@ public:
   void        lock();
   cmap*       getMap();
   const cmap* getMap() const;
+  void        setCTick( int newValue );
+  void        setTicker( std::size_t newValue );
+
+  void tick();
 
 private:
+  int speedstep( int n, int d, int s, int update );
+  int interpolateItemSprite( int mapIndex );
+  int interpolateCharacterSprite( int mapIndex );
+  int speedo( int n );
+
   std::unique_ptr< cmap[] > map_;
   std::mutex                mapMutex_;
+  int                       ctick_;
+  std::size_t               ticker_;
 };
 } // namespace MenAmongGods
 
