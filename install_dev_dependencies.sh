234770ba93dc33e1d59e5c4046da0fa29f74c3f2
James Armes <jamesleearmes@gmail.com>
2022-02-24 22:25:20 -0700

    Add machinery for sanitizers

--- install_dev_dependencies.sh
+++ install_dev_dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Installing development dependencies for SFML..."
-sudo apt-get install -y libx11-dev libxrandr-dev libopengl-dev libgl-dev libfreetype-dev libopenal-dev libvorbis-dev libflac-dev gdb libboost-all-dev libzip-dev
+sudo apt-get install -y libx11-dev libxrandr-dev libopengl-dev libgl-dev libfreetype-dev libopenal-dev libvorbis-dev libflac-dev gdb libboost-all-dev libzip-dev libubsan1 libasan6
