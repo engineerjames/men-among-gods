874c1642d1162a22c2e4daa2f22485c7371f09d9
James Armes <jamesleearmes@gmail.com>
2022-03-09 19:14:46 -0700

    Added ability to edit name and description

--- src/game-designer/src/CharacterDetails.tsx
+++ src/game-designer/src/CharacterDetails.tsx
@@ -1,5 +1,5 @@
 import { Checkbox, Divider, FormControlLabel, Stack, TextField } from '@mui/material';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CharacterAttributes, { CharacterAttribute } from './CharacterAttributes';
 import CharacterFlags from './CharacterFlags';
 import CharacterSkills, { CharacterSkill } from './CharacterSkills';
@@ -35,16 +35,29 @@ export interface CharacterDetailProps {
 }
 
 const CharacterDetails = ({ details, loaded }: CharacterDetailProps) => {
-
     const positionString = `( ${details?.x}, ${details?.y} )`;
     const lifeString = `${details?.hp?.at(0)} / ${details?.end?.at(0)} / ${details?.mana?.at(0)}`;
+
+    const [name, setName] = useState(details?.name);
+    const [description, setDescription] = useState(details?.description);
+
+    useEffect(() => {
+        if (details?.name) {
+            details.name = name;
+        }
+
+        if (details?.description) {
+            details.description = description;
+        }
+    }, [name, description])
+
 
     if (loaded) {
         return (<>
             <Stack spacing={2}>
                 <TextField id="filled-basic" label='Id' value={details?.id} variant="filled" InputProps={{ readOnly: true }} />
-                <TextField id="filled-basic" label='Name' value={details?.name} variant="filled" InputProps={{ readOnly: true }} />
-                <TextField id="filled-basic" label='Description' value={details?.description} variant="filled" InputProps={{ readOnly: true }} />
+                <TextField id="filled-basic" label='Name' onChange={(event) => { setName(event.target.value); }} value={name} variant="filled" />
+                <TextField id="filled-basic" label='Description' onChange={(event) => { setDescription(event.target.value); }} value={description} variant="filled" />
 
                 <Stack spacing={3} direction="row" divider={<Divider orientation="vertical" flexItem />}>
                     <FormControlLabel control={<Checkbox />} label='Is Used' checked={details?.used === 1} />
