3947ce6a4a4d46dffdf82d24e23e82dcea13321d
James Armes <jamesleearmes@gmail.com>
2022-03-09 19:50:27 -0700

    Re-work attribute display

--- src/game-designer/src/CharacterAttributes.tsx
+++ src/game-designer/src/CharacterAttributes.tsx
@@ -1,4 +1,4 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import React from 'react'
 
 export interface CharacterAttribute {
@@ -10,6 +10,76 @@ export interface CharacterAttributesProps {
     attributes?: Array<CharacterAttribute>;
 }
 
+const columns: GridColDef[] = [
+    {
+        field: 'id',
+        headerName: 'Name',
+        headerAlign: 'center',
+        width: 100,
+        align: 'left',
+        sortable: false,
+        editable: false
+    },
+    {
+        field: 'baseValue',
+        headerName: 'Base',
+        headerAlign: 'center',
+        description: 'Base value of the attribute.',
+        width: 100,
+        align: 'center',
+        sortable: false,
+        editable: true,
+    },
+    {
+        field: 'presetModifier',
+        headerName: 'Preset',
+        headerAlign: 'center',
+        description: 'Preset modifier; race/npc dependent.',
+        width: 100,
+        align: 'center',
+        sortable: false,
+        editable: true,
+    },
+    {
+        field: 'maxValue',
+        headerName: 'Max',
+        headerAlign: 'center',
+        description: 'Race specific maximum.',
+        type: 'number',
+        align: 'center',
+        width: 100,
+        sortable: false,
+        editable: true,
+    },
+    {
+        field: 'difficultyToRaise',
+        headerName: 'Difficulty',
+        headerAlign: 'center',
+        description: 'Race specific difficulty to raise.',
+        align: 'center',
+        sortable: false,
+        width: 100,
+    },
+    {
+        field: 'dynamicModifier',
+        headerName: 'Modifier',
+        headerAlign: 'center',
+        description: 'Dynamic modifier based on equipment and/or spells.',
+        align: 'center',
+        sortable: false,
+        width: 100,
+    },
+    {
+        field: 'total',
+        headerName: 'Total',
+        headerAlign: 'center',
+        description: 'Total modified value--typically not stored in templates.',
+        align: 'center',
+        sortable: false,
+        width: 100,
+    },
+];
+
 // character stats
 // [0]=bare value, 0=unknown
 // [1]=preset modifier, is race/npc dependend
@@ -17,46 +87,34 @@ export interface CharacterAttributesProps {
 // [3]=race specific difficulty to raise (0=not raisable, 1=easy ... 10=hard)
 // [4]=dynamic modifier, depends on equipment and spells
 // [5]=total value
-function CharacterAttributes(props: CharacterAttributesProps) {
-    return (
-        <>
-        <h3>Character Attributes</h3>
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Attribute Name</TableCell>
-                        <TableCell align="right">Base Value</TableCell>
-                        <TableCell align="right">Preset Modifier</TableCell>
-                        <TableCell align="right">Maximum Value</TableCell>
-                        <TableCell align="right">Difficulty to Raise [0-10]</TableCell>
-                        <TableCell align="right">Dynamic Modifier (equip + spells)</TableCell>
-                        <TableCell align="right">Total</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {props?.attributes?.map((attr) => (
-                        <TableRow
-                            key={attr.name}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell component="th" scope="row">
-                                {attr.name}
-                            </TableCell>
-                            <TableCell align="right">{attr.data[0]}</TableCell>
-                            <TableCell align="right">{attr.data[1]}</TableCell>
-                            <TableCell align="right">{attr.data[2]}</TableCell>
-                            <TableCell align="right">{attr.data[3]}</TableCell>
-                            <TableCell align="right">{attr.data[4]}</TableCell>
-                            <TableCell align="right">{attr.data[5]}</TableCell>
 
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-        </>
-    )
+function CharacterAttributes(props: CharacterAttributesProps) {
+
+    const rows: any = [];
+    props?.attributes?.map((attr) => {
+        console.log(attr);
+        rows.push({
+            id: attr.name,
+            baseValue: attr.data[0],
+            presetModifier: attr.data[1],
+            maxValue: attr.data[2],
+            difficultyToRaise: attr.data[3],
+            dynamicModifier: attr.data[4],
+            total: attr.data[5]
+        })
+    });
+
+    return (
+        <div style={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+                disableSelectionOnClick
+            />
+        </div>
+    );
 }
 
 export default CharacterAttributes
