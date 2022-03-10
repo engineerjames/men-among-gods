c3876ec288cb2821927f0ce36f3bc690a0aeefbe
James Armes <jamesleearmes@gmail.com>
2022-03-10 10:56:37 -0700

    Got ipcRenderer to finally load

--- src/new_client/electron_app/men-among-gods/public/electron.js
+++ src/new_client/electron_app/men-among-gods/public/electron.js
@@ -6,15 +6,16 @@ const isDev = require('electron-is-dev');
 function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 575,
-    height: 575,
+    width: 600,
+    height: 709,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false,
     },
   });
 
   // and load the index.html of the app.
-  // win.loadFile("index.html");
+  win.loadFile("index.html");
   win.loadURL(
     isDev
       ? 'http://localhost:3000'
