4502d600424863ce4211d510909f94bfc54f7383
James Armes <jamesleearmes@gmail.com>
2022-03-11 16:11:31 -0700

    Add MOA file loading

--- src/new_client/electron_app/men-among-gods/src/App.tsx
+++ src/new_client/electron_app/men-among-gods/src/App.tsx
@@ -1,9 +1,79 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import SexSelection, { Sex } from './SexSelection';
 import RaceSelection, { Race } from './RaceSelection';
 import CharacterInput from './CharacterInput';
 import { Box, Button, Divider } from '@mui/material';
+
+// This is essentially duplicated logic from ClientTypes.h
+const intToRaceAndSex = (kindred: number): [Race, Sex] => {
+  switch ( kindred )
+  {
+  case 3:
+    return [Race.Templar, Sex.Male];
+  case 2:
+    return [Race.Mercenary, Sex.Male ];
+  case 4:
+    return [Race.Harakim, Sex.Male ];
+  case 13:
+    return [Race.Seyan, Sex.Male ];
+  case 543:
+    return [Race.God, Sex.Male ];
+  case 544:
+    return [Race.ArchTemplar, Sex.Male ];
+  case 545:
+    return [Race.ArchHarakim, Sex.Male ];
+  case 546:
+    return [Race.Sorceror, Sex.Male ];
+  case 547:
+    return [Race.Warrior, Sex.Male ];
+  case 77:
+    return [Race.Templar, Sex.Female ];
+  case 76:
+    return [Race.Mercenary, Sex.Female ];
+  case 78:
+    return [Race.Harakim, Sex.Female ];
+  case 79:
+    return [Race.Seyan, Sex.Female ];
+  case 548:
+    return [Race.God, Sex.Female];
+  case 549:
+    return [Race.ArchTemplar, Sex.Female ];
+  case 550:
+    return [Race.ArchHarakim, Sex.Female ];
+  case 551:
+    return [Race.Sorceror, Sex.Female ];
+  case 552:
+    return [Race.Warrior, Sex.Female ];
+  default:
+    return [Race.Templar, Sex.Male ];
+  }
+}
+
+interface xbutton {
+  name: string;
+  skill_id: number;
+}
+
+interface key {
+  name: string;
+  pass1: number;
+  pass2: number;
+  race: number;
+  usnr: number;
+}
+
+interface MOAFile {
+  pdata: {
+    name: string;
+    desc: string;
+    ref: string;
+    show_names: number;
+    show_percent_health: number;
+    xbutton: Array<xbutton>;
+  };
+  key: key;
+}
 
 function App() {
 
@@ -12,6 +82,7 @@ function App() {
   const [name, setName] = useState('');
   const [desc, setDesc] = useState('');
   const [pass, setPass] = useState('');
+  const [moaFile, setMoaFile] = useState<MOAFile>();
 
   const clearState = () => {
     setSex(Sex.None);
@@ -20,6 +91,23 @@ function App() {
     setDesc('');
     setPass('');
   }
+
+  useEffect(() => {
+    if (moaFile) {
+      console.log('Setting name to' + moaFile.pdata.name);
+      setName(moaFile.key.name);
+      setDesc(moaFile.pdata.desc);
+
+      // Now set the sex and race
+      let [race, sex] = intToRaceAndSex(moaFile.key.race);
+      setSex(sex);
+      setRace(race);
+    }
+    else {
+      console.log('Invalid MOA file: ' + moaFile);
+    }
+  }, [moaFile])
+
 
   const onClickHandler = () => {
     const dialogConfig = {
@@ -30,14 +118,14 @@ function App() {
 
     // @ts-ignore: TS isn't aware of context 
     window.electron.openDialog('showOpenDialog', dialogConfig)
-    // @ts-ignore: TS isn't aware of context 
-      .then((result) => { 
-        console.log('The result is: ' + result); 
-        console.log(result);
-        
-        console.log('READING FILE - CLIENT')
+      // @ts-ignore: TS isn't aware of context 
+      .then((result) => {
         // @ts-ignore: TS isn't aware of context 
-        window.electron.readFile(result.filePaths[0]); 
+        window.electron.readFile(result.filePaths[0]).then((result2) => {
+          let moaFileLoaded: MOAFile = JSON.parse(result2) as MOAFile;
+          console.log(moaFileLoaded);
+          setMoaFile(moaFileLoaded);
+        })
       });
 
   }
--- src/new_client/electron_app/men-among-gods/src/preload.ts
+++ src/new_client/electron_app/men-among-gods/src/preload.ts
@@ -1,7 +1,5 @@
 import { contextBridge, ipcRenderer } from "electron";
 
-console.log('PRELOADED');
-
 contextBridge.exposeInMainWorld('electron', {
     openDialog: (method: any, config: any) => ipcRenderer.invoke('dialog', method, config),
     readFile: (method: any, config : any) => ipcRenderer.invoke('readSelectedFile', method, config),
