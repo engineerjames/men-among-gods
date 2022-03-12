141065dc46006060c7bfa3a6dba48b6e99dade4b
James Armes <jamesleearmes@gmail.com>
2022-03-11 23:00:32 -0700

    Add known output location releative to electron app

--- src/new_client/electron_app/men-among-gods/src/App.tsx
+++ src/new_client/electron_app/men-among-gods/src/App.tsx
@@ -132,7 +132,7 @@ function App() {
   }
 
   let loadGame = () => {
-    let pathToGame: string = '/home/jarmes/git/men-among-gods/out/build/WSL-GCC9-Debug/src/new_client/MenAmongGods';
+    let pathToGame: string = './client/MenAmongGods.exe';
 
     if (moaFile) {
       // @ts-ignore: TS isn't aware of context 
