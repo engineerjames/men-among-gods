0aaa7707d1374f640d8af6830856d396eaaeb252
James Armes <jamesleearmes@gmail.com>
2023-07-28 12:18:33 -0600

    Add .lock files, and the CharacterDetails function component

---
+++ src/tauri_frontend/src/CharacterDetails.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+
+interface Props {
+
+}
+
+const CharacterDetails : FunctionComponent<Props> = () => {
+    return <>
+        Character Deets
+    </>;
+}
+
+export default CharacterDetails;
