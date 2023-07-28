3f31b4ea5bc798d53fc07d713ee333a53da28722
James Armes <jamesleearmes@gmail.com>
2023-07-28 17:19:29 -0600

    Remove unnecessary imports

--- src/tauri_frontend/src/App.tsx
+++ src/tauri_frontend/src/App.tsx
@@ -1,17 +1,7 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import CharacterDetails from "./CharacterDetails";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return <>
     <div className="container">
       <h1>Men Among Gods</h1>
