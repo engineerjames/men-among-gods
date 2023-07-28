782643b882d7b2e50be6b5bccda2c99c0a96c6eb
James Armes <jamesleearmes@gmail.com>
2023-07-28 17:19:09 -0600

    Add basic UI implementation

--- src/tauri_frontend/src-tauri/src/main.rs
+++ src/tauri_frontend/src-tauri/src/main.rs
@@ -7,9 +7,14 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn play(name: &str, pass: &str) {
+    println!("Playing with {}, and {}", name, pass);
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, play])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/tauri_frontend/src/CharacterDetails.tsx
+++ src/tauri_frontend/src/CharacterDetails.tsx
@@ -1,12 +1,39 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+import { Stack, Divider, CircularProgress, Backdrop } from "@mui/material";
 
-interface Props {
-
+async function play(name: string, pass: string) {
+    await invoke("play", { name: name, pass: pass });
 }
 
-const CharacterDetails : FunctionComponent<Props> = () => {
+const CharacterDetails: FunctionComponent<{}> = () => {
+    const [name, setName] = useState("");
+    const [pass, setPass] = useState("");
+
+    const [buttonsAreEnabled, setbuttonsAreEnabled] = useState(true);
+
     return <>
-        Character Deets
+        <Stack spacing={2}>
+            <input hidden={!buttonsAreEnabled} value={name} onChange={(e) => setName(e.currentTarget.value)} required id="outlined-basic" placeholder="Character Name" />
+            <input hidden={!buttonsAreEnabled} value={pass} onChange={(e) => setPass(e.currentTarget.value)} required id="outlined-basic" placeholder="Password" type="password" />
+
+            <Stack spacing={2} direction="row" divider={<Divider orientation="vertical" flexItem />} >
+                <button hidden={!buttonsAreEnabled} onClick={() => {
+                    setbuttonsAreEnabled(false);
+                    play(name, pass);
+                }}>Play</button>
+                <button hidden={!buttonsAreEnabled} onClick={() => {
+                    setName("");
+                    setPass("");
+                }}>New</button>
+            </Stack>
+        </Stack>
+        <Backdrop
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={!buttonsAreEnabled}
+        >
+            <CircularProgress color="inherit" />
+        </Backdrop>
     </>;
 }
 
