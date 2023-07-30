d27272a0866542a5234c8767bb152df903048a05
James Armes <jamesleearmes@gmail.com>
2023-07-30 12:57:38 -0600

    Add in packaging of build outputs to client_files

--- create_release_packages.ps1
+++ create_release_packages.ps1
@@ -1,5 +1,6 @@
+cmake --build C:/git/men-among-gods/out/build/MSVC-vcpkg-release --parallel 22 --target create_release
 New-Item -Force -Type Directory src/tauri_frontend/resources
-Copy-Item -Force -Recurse ./out/build/MSVC-vcpkg-debug/client_files/* src/tauri_frontend/resources
+Copy-Item -Force -Recurse ./out/build/MSVC-vcpkg-release/client_files/* src/tauri_frontend/resources
 Set-Location -Path ./src/tauri_frontend
 pnpm run tauri build
 Set-Location -Path ../../
--- create_release_packages.sh
+++ create_release_packages.sh
@@ -1,3 +1,4 @@
+cmake --build C:/git/men-among-gods/out/build/MSVC-vcpkg-release --parallel 22 --target create_release
 mkdir -p ./src/tauri_frontend/resources
 # cp -R ./out/ <-- Get Build directory
 cd ./src/tauri_frontend
